c8e41d0e276b0acfd4be0e30518e29a5ccb476a4
Cecília Nascimento <48659501+nascimentocrp@users.noreply.github.com>
2020-06-22 20:52:55 -0300

    Update removerCaracteresEspeciaisEAcentos.py

--- removerCaracteresEspeciaisEAcentos.py
+++ removerCaracteresEspeciaisEAcentos.py
@@ -2,7 +2,8 @@ from unicodedata import normalize
 import re
 
 """
-Script para remover acentos e caracteres especiais de um texto usando python. Caracteres com acento serão substituidos para o equivalente sem acentuação e o ç será trocado por c.
+Script para remover acentos e caracteres especiais de um texto usando python. Caracteres com acento 
+serão substituidos para o equivalente sem acentuação e o ç será trocado por c.
 
 Para NÃO passar os caracteres para minusculo, basta retirar o "lower.()" do metodo.
 """
